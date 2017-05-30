374ed1ef35dfeeff2b034e57bf15fdcecf3b4026
Roman Laktionov <andr.kisilevskiy@gmail.com>
2017-05-30 20:06:45 +0300

    Changed host

--- inproject-categories/server.js
+++ inproject-categories/server.js
@@ -6,7 +6,7 @@ new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(8181, '0.0.0.0', function (err, result) {
+}).listen(8181, 'juicydeli.com', function (err, result) {
   if (err) {
     console.log(err);
   }
